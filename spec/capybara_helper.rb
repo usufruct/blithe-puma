2d843ca0bd5066ee1f67e8f35c058528ad81fb58
Michael A Anderson <usufruct@gmail.com>
2017-11-06 12:00:59 -0600

    'headless' chrome webdriver target added

--- spec/capybara_helper.rb
+++ spec/capybara_helper.rb
@@ -1,4 +1,21 @@
 require 'capybara/rspec'
+require 'selenium/webdriver'
 require './lib/simple_app'
 
 Capybara.app = SimpleApp.new
+
+Capybara.register_driver :chrome do |app|
+    Capybara::Selenium::Driver.new(app, browser: :chrome)
+end
+
+Capybara.register_driver :headless_chrome do |app|
+    capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+        chromeOptions: { args: %w(headless disable-gpu) }
+    )
+
+    Capybara::Selenium::Driver.new app,
+        browser: :chrome,
+        desired_capabilities: capabilities
+end
+
+Capybara.javascript_driver = :chrome
--- spec/integration/herman_spec.rb
+++ spec/integration/herman_spec.rb
@@ -8,11 +8,21 @@ RSpec.feature "does it even work?" do
 end
 
 RSpec.feature "route: /herman" do
-   describe "loading the page" do
-       it "loads" do
-           srand(999) # page returns randon content
-           visit '/herman'
-           expect(page).to have_text('the threatening wind forbade.')
-       end
-   end
+    before(:each) do
+        Capybara.current_driver = :headless_chrome
+    end
+
+    after(:each) do
+        Capybara.use_default_driver
+    end
+
+    describe "loading the page" do
+        it "loads" do
+            puts "see js: #{Capybara.javascript_driver}"
+            puts "see: #{Capybara.current_driver}"
+            srand(999) # page returns randon content
+            visit '/herman'
+            expect(page).to have_text('the threatening wind forbade.')
+        end
+    end
 end
